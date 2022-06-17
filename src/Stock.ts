9bf33d4c365ffb517d2397a588814e4558fec7e0
Angel Dominguez <angeldomp49@gmail.com>
2022-06-16 19:42:14 -0500

    defaultUrls

--- src/Stock.ts
+++ src/Stock.ts
@@ -4,38 +4,51 @@ import { Category } from "./Category";
 
 export class Stock{
 
-    url: string;
-
-    go(url: string){
-        this.url = url;
-        return this;
+    host: string = "http://localhost:8000";
+    defaultUrls = {
+        productList: () => this.host+"/api/products",
+        productsByTag: (tag: Tag) => this.host+`/api/products/tag/${tag.id}`,
+        productsChunk: (page: number, size: number) => this.host+`/api/products/chunk/${page}/${size}`,
+        productsByCategory: (category: Category) => this.host+`/api/products/category/${category.id}`,
+        productById: (id: number) => this.host+`/api/products/${id}`,
+        productsBySearch: (search: string) => this.host+`/api/products/?search=${search}`,
     }
 
-    productsByTag(onSuccess: Function, tag: Tag, makeUrl: Function = defaultUrls.productsByTag){
+    constructor(host: string = "http://localhost:8000", defaultUrls: any = {}){
+        this.host = host;
+        this.defaultUrls = {...this.defaultUrls, ...defaultUrls};
+    }
+
+
+    productList(onSuccess: Function, makeUrl: Function = this.defaultUrls.productList){
+        this.simpleGet(makeUrl(), onSuccess);
+    }
+
+    productsByTag(onSuccess: Function, tag: Tag, makeUrl: Function = this.defaultUrls.productsByTag){
         this.simpleGet(makeUrl(tag), onSuccess);
     }
 
-    productsChunk(page: number, size: number, onSuccess: Function, makeUrl: Function = defaultUrls.productsChunk){
+    productsChunk(page: number, size: number, onSuccess: Function, makeUrl: Function = this.defaultUrls.productsChunk){
         this.simpleGet(makeUrl(page, size), onSuccess);
     }
 
-    productsByCategory(onSuccess: Function, category: Category, makeUrl: Function = defaultUrls.productsByCategory){
+    productsByCategory(onSuccess: Function, category: Category, makeUrl: Function = this.defaultUrls.productsByCategory){
         this.simpleGet(makeUrl(category), onSuccess);
     }
 
-    productById(id: number, onSuccess: Function, makeUrl: Function = defaultUrls.productById){
+    productById(id: number, onSuccess: Function, makeUrl: Function = this.defaultUrls.productById){
         this.simpleGet(makeUrl(id), onSuccess);
     }
 
-    productsBySearch(search: string, onSuccess: Function, makeUrl: Function = defaultUrls.productsBySearch){
+    productsBySearch(search: string, onSuccess: Function, makeUrl: Function = this.defaultUrls.productsBySearch){
         let searchEncoded = encodeURIComponent(search);
         this.simpleGet(makeUrl(searchEncoded), onSuccess);
     }
 
     simpleGet(url: string, onSuccess: Function){
         axios.get(url).then(response => {
-            let data = JSON.parse(response.data);
-            onSuccess(response.data);
+            let data = response.data;
+            onSuccess(data);
         }).catch(error => {
             this.handleError(error);
         });
@@ -44,12 +57,4 @@ export class Stock{
     handleError(error: any){
         console.log(error);
     }
-}
-
-const defaultUrls = {
-    productsByTag: (tag: Tag) => `/api/products/tag/${tag.id}`,
-    productsChunk: (page: number, size: number) => `/api/products/chunk/${page}/${size}`,
-    productsByCategory: (category: Category) => `/api/products/category/${category.id}`,
-    productById: (id: number) => `/api/products/${id}`,
-    productsBySearch: (search: string) => `/api/products/?search=${search}`,
 }
